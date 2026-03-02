7926f96ce68a99a53d87fbb08324f4725e9cbdd6
Moustafa-FD <moustafafadlalla@gmail.com>
2026-03-02 07:08:06 -0500

    cleanup

--- auth/src/routes/auth.route.ts
+++
@@ -1,7 +0,0 @@
-const auth = require('../controllers/auth.controller');
-
-module.exports = (app: any) => {
-    app.post('/signin/admin', auth.signInHandler(true));
-    app.post('/signin/student', auth.signInHandler(false));
-    app.get('/signout', auth.signout);
-};
